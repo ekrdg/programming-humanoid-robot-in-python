1e5705801a54e92104fdf2a0da94cd57518290a8
ekrdg <e.karadag@outlook.de>
2023-12-03 20:57:22 +0100

    Update agent_server.py

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,6 +14,8 @@
 # add PYTHONPATH
 import os
 import sys
+import threading
+from xmlrpc.server import SimpleXMLRPCServer
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
@@ -22,35 +24,48 @@ from inverse_kinematics import InverseKinematicsAgent
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
-    # YOUR CODE HERE
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+        self.server = SimpleXMLRPCServer(('localhost', 8000), allow_none=True)
+        self.server.register_introspection_functions()
+        self.server.register_multicall_functions()
+        self.server.register_instance(self)
+        self.thread = threading.Thread(target=self.server.serve_forever)
+        self.thread.start()
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
+        return self.perception.joint.get(joint_name)
         # YOUR CODE HERE
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
+        self.target_joints[joint_name]= angle
         # YOUR CODE HERE
 
     def get_posture(self):
         '''return current posture of robot'''
+        #not done 
         # YOUR CODE HERE
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
+        self.keyframes= keyframes
         # YOUR CODE HERE
 
     def get_transform(self, name):
         '''get transform with given name
         '''
+        return self.transforms[name]
         # YOUR CODE HERE
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
+        self.transforms[effector_name]= transform
         # YOUR CODE HERE
 
 if __name__ == '__main__':
